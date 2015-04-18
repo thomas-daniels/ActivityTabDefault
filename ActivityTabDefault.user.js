b5c987997dd09f6e0916268a934868f4f4f901e8
ProgramFOX <programfox@hotmail.be>
2015-04-18 14:31:12 +0200

    Initial commit

---
+++ ActivityTabDefault.user.js
@@ -0,0 +1,15 @@
+// ==UserScript==
+// @name        'Activity' tab as default
+// @namespace   http://stackexchange.com/users/3094403/programfox
+// @description A userscript that automatically shows the Activity tab as default on profile pages
+// @include     /^https?:\/\/[a-zA-Z0-9\.]+\.stackexchange\.com\/users\/[0-9]+\/[^?]+$/
+// @include     /^https?:\/\/((www|meta)\.)?stackoverflow\.com\/users\/[0-9]+\/[^?]+$/
+// @include     /^https?:\/\/((www|meta)\.)?superuser\.com\/users\/[0-9]+\/[^?]+$/
+// @include     /^https?:\/\/((www|meta)\.)?serverfault\.com\/users\/[0-9]+\/[^?]+$/
+// @include     /^https?:\/\/((www|meta)\.)?askubuntu\.com\/users\/[0-9]+\/[^?]+$/
+// @include     /^https?:\/\/((www|meta)\.)?mathoverflow\.net\/users\/[0-9]+\/[^?]+$/
+// @version     1
+// @grant       none
+// ==/UserScript==
+
+window.location.href += "?tab=topactivity";
---
+++ README.md
@@ -0,0 +1,3 @@
+# 'Activity' tab as default
+
+This userscript redirects you by default to the 'Activity' tab of a Stack Exchange profile, if you have not selected another tab.
