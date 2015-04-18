2f52991dc39bcf3b046121b703be06c1024b5bba
ProgramFOX <programfox@hotmail.be>
2015-04-18 14:41:41 +0200

    Add author

--- ActivityTabDefault.user.js
+++ ActivityTabDefault.user.js
@@ -1,6 +1,7 @@
 // ==UserScript==
 // @name        'Activity' tab as default
 // @namespace   http://stackexchange.com/users/3094403/programfox
+// @author      ProgramFOX
 // @description A userscript that automatically shows the Activity tab as default on profile pages
 // @include     /^https?:\/\/[a-zA-Z0-9\.]+\.stackexchange\.com\/users\/[0-9]+\/[^?]+$/
 // @include     /^https?:\/\/((www|meta)\.)?stackoverflow\.com\/users\/[0-9]+\/[^?]+$/
