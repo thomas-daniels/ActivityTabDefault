73f6b0c9cb37ba7389b16f4d16cc7d485fee8f4c
ProgramFOX <programfox@hotmail.be>
2015-04-18 16:33:54 +0200

    More performance
    http://stackapps.com/a/6268/25384

--- ActivityTabDefault.user.js
+++ ActivityTabDefault.user.js
@@ -3,15 +3,21 @@
 // @namespace   http://stackexchange.com/users/3094403/programfox
 // @author      ProgramFOX
 // @description A userscript that automatically shows the Activity tab as default on profile pages
-// @include     /^https?:\/\/[a-zA-Z0-9\.]+\.stackexchange\.com\/users\/[0-9]+\/[^?]+$/
-// @include     /^https?:\/\/((www|meta)\.)?stackoverflow\.com\/users\/[0-9]+\/[^?]+$/
-// @include     /^https?:\/\/((www|meta)\.)?superuser\.com\/users\/[0-9]+\/[^?]+$/
-// @include     /^https?:\/\/((www|meta)\.)?serverfault\.com\/users\/[0-9]+\/[^?]+$/
-// @include     /^https?:\/\/((www|meta)\.)?askubuntu\.com\/users\/[0-9]+\/[^?]+$/
-// @include     /^https?:\/\/((www|meta)\.)?mathoverflow\.net\/users\/[0-9]+\/[^?]+$/
+// @match       *://*.stackexchange.com/users/*
+// @match       *://*.stackoverflow.com/users/*
+// @match       *://*.superuser.com/users/*
+// @match       *://*.serverfault.com/users/*
+// @match       *://*.askubuntu.com/users/*
+// @match       *://*.mathoverflow.net/users/*
+// @match       *://*.stackapps.com/users/*
+// @match       *://answers.onstartups.com/users/*
 // @version     1
 // @grant       none
 // @run-at      document-start
 // ==/UserScript==
 
-window.location.replace(window.location.href + "?tab=topactivity");
+if (!window.location.search &&
+    !window.location.hash &&
+    !(/\/users\/edit\//i).test(window.location.pathname)) {
+    window.location.replace(window.location.href + "?tab=topactivity");
+}
